223e76d6dbfdfbbea8064787c1898c89b6e0a642
Vazgen Petrosian <ekaterina.vanshet@MacBook-Air-Ekaterina-2.local>
2023-11-10 16:55:26 +0300

    Tryy

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
@@ -19,6 +20,12 @@ app.use(helmet());
 
 
 app.use(requestLogger); // подключаем логгер запросов
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 
 app.use(router);
 
