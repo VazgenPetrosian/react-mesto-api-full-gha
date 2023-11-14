fe2a62367333a995c27c6ea7c90201bb3881da8b
Vazgen Petrosian <ekaterina.vanshet@MacBook-Air-Ekaterina-2.local>
2023-11-14 18:39:41 +0300

    try with api feat card

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,7 +5,7 @@ const NotFound = require('../errors/NotFoundError');
 const { loginUser, createUser } = require('../controllers/user');
 
 router.use('/api/users', auth, require('./userRouter'));
-router.use('/api//cards', auth, require('./cardRouter'));
+router.use('/api/cards', auth, require('./cardRouter'));
 
 router.use('/api/signin', validateLogin, loginUser);
 router.use('/api/signup', validateRegistration, createUser);
